cb382df40d3ebb86186072782353bddb9b494367
Darren Batarina <batarina.darren@gmail.com>
2026-03-28 10:44:02 +1100

    init

---
+++ src/main/java/io/darbata/zerotrust/ZeroTrustApplication.java
@@ -0,0 +1,13 @@
+package io.darbata.zerotrust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroTrustApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroTrustApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/darbata/zerotrust/ZeroTrustApplicationTests.java
@@ -0,0 +1,13 @@
+package io.darbata.zerotrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroTrustApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
